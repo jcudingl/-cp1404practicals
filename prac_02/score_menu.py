d4668e16e37cd0bef789f676e5700d05f877a53c
jcudingl <ding.li@my.jcu.edu.au>
2024-02-25 18:05:39 +0800

    Fix the problem that if input such as "41" as score and convert to float type, it will store it as 41.0 and cause the length of the score incorrect.

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,21 +1,51 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
 """
+
+MENU = """(G)et a valid score
+(P)rint result
+(S)how stars
+(Q)uit"""
 
 
 def main():
-    score = float(input("Enter score: "))
-    status = determine_status(score)
-    print(status)
+    score = get_valid_score()
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            status = determine_status(score)
+            print(status)
+        elif choice == "S":
+            show_stars(score)
+        else:
+            print("Invalid option")
+        print()
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def show_stars(score):
+    for i in range(len(score)):
+        print("*", end='')
+    print()
+
+
+def get_valid_score():
+    score = input("Enter score: ")
+    while float(score) < 0 or float(score) > 100:
+        print("Invalid score")
+        score = input("Enter score: ")
+    return score
 
 
 def determine_status(score):
-    if score < 0 or score > 100:
-        return "Invalid score"
-    if score >= 90:
+    if float(score) >= 90:
         return "Excellent"
-    if score >= 50:
+    if float(score) >= 50:
         return "Passable"
     return "Bad"
 
