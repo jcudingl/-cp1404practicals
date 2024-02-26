16b4fd3d645990c1c52e159c55fd19aa30b7d6ad
jcudingl <ding.li@my.jcu.edu.au>
2024-02-26 15:10:55 +0800

    Add code to print the result in a file.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -14,10 +14,12 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01  # $0.01 -> $1
 MAX_PRICE = 1000.0  # $1000 -> $100
 INITIAL_PRICE = 10.0
+OUTPUT_FILE = "capitalist_conrad.txt"
 
+out_file = open(OUTPUT_FILE, 'w')
 price = INITIAL_PRICE
 number_of_days = 0
-print(f"Starting price: ${price:,.2f}")
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -34,4 +36,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
 
     price *= (1 + price_change)
     number_of_days += 1
-    print(f"On day {number_of_days} price is: ${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+out_file.close()
