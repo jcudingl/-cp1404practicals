bd2f8d1c8ab1f2f63f82753d5d61d43e69df35e6
jcudingl <ding.li@my.jcu.edu.au>
2024-02-26 15:18:52 +0800

    Answered Q3 by adding an error check for denominator.

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -4,12 +4,20 @@ Answer the following questions:
 1. When will a ValueError occur?
 When input invalid literal for int().
 2. When will a ZeroDivisionError occur?
+When denominator is 0.
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+To add an error check for denominator: if the input is 0, return invalid input and ask for a new input.
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+
+    # Error check added
+    while denominator == 0:
+        print("Cannot divide by zero!")
+        denominator = int(input("Enter the denominator: "))
+
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
