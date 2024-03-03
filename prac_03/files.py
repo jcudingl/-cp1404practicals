e6322cf3543239f7292f3cd067673fd9b0d28e0f
jcudingl <ding.li@my.jcu.edu.au>
2024-03-03 21:16:54 +0800

    Finish question 2.

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,6 +1,14 @@
-FILE_NAME = "name.txt"
+FILE_NAME_Q1 = "name.txt"
 
+# Question 1
 user_name = input("Name: ")
-out_file = open(FILE_NAME, "w")
+out_file = open(FILE_NAME_Q1, "w")
 print(user_name, file=out_file)
 out_file.close()
+
+# Question 2
+in_file = open(FILE_NAME_Q1, "r")
+name = in_file.read().strip()
+print(f"Your name is {name}.")
+in_file.close()
+
