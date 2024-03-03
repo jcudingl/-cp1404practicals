55845478c3664fafe0140216615942caa89e9ccd
jcudingl <ding.li@my.jcu.edu.au>
2024-03-03 21:27:19 +0800

    Finish Question 4.

--- prac_03/files.py
+++ prac_03/files.py
@@ -7,6 +7,7 @@ user_name = input("Name: ")
 out_file = open(FILE_NAME_Q1, "w")
 print(user_name, file=out_file)
 out_file.close()
+print()
 
 # Question 2
 print("Python File Question 2")
@@ -14,6 +15,7 @@ in_file = open(FILE_NAME_Q1, "r")
 name = in_file.read().strip()
 print(f"Your name is {name}.")
 in_file.close()
+print()
 
 # Question 3
 print("Python File Question 3")
@@ -22,4 +24,15 @@ first_number = int(in_file.readline().strip())
 second_number = int(in_file.readline().strip())
 total = first_number + second_number
 print(f"The total of first two numbers is {total}.")
+in_file.close()
+print()
 
+# Question 4
+print("Python File Question 4")
+in_file = open(FILE_NAME_Q3, "r")
+total = 0
+for line in in_file:
+    total += int(line)
+print(f"The total numbers is {total}.")
+in_file.close()
+print()
