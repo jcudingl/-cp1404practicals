563dd541c5a2e99eee3ced8768608c2f4038b0da
jcudingl <ding.li@my.jcu.edu.au>
2024-03-04 14:49:18 +0800

    Complete all questions.

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -36,3 +36,15 @@ numbers + [6, 5, 3]
 # My answer: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 # Result in Python Console: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 # Correct
+
+# Q1
+numbers[0] = "ten"
+
+# Q2
+numbers[-1] = 1
+
+# Q3
+print(numbers[2:])
+
+# Q4
+print(9 in numbers)
