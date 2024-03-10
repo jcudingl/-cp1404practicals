8424188cda641ff179bf8557a1ccb11b85fc58d7
jcudingl <ding.li@my.jcu.edu.au>
2024-03-10 22:12:46 +0800

    Finish.

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,4 +1,14 @@
+import random
+
 MINIMUM_NUMBER = 1
 MAXIMUM_NUMBER = 45
 NUMBER_IN_A_LINE = 6
 
+numbers = list(range(MINIMUM_NUMBER, MAXIMUM_NUMBER + 1))
+number_of_quick_pick = int(input("How many quick picks? "))
+for line in range(number_of_quick_pick):
+    random.shuffle(numbers)
+    quick_pick = sorted(numbers[:NUMBER_IN_A_LINE])
+    for number in quick_pick:
+        print(f"{number:2} ", end="")
+    print()
