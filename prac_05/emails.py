49e0c5830eefa6ecffd1b2824968bdb15d51a111
jcudingl <ding.li@my.jcu.edu.au>
2024-03-17 21:05:10 +0800

    Finish Code.

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -3,3 +3,16 @@ Emails
 Estimate: 10 minutes
 Actual:
 """
+email_dictionary = {}
+email = input("Email: ")
+while email != "":
+    name = "".join(word.capitalize() for word in email.split("@")[0].split("."))
+    check = input(f"Is your name {name}? (Y/n)")
+    if check == "Y" or check == "":
+        email_dictionary[name] = email
+    else:
+        name = input("Name: ")
+        email_dictionary[name] = email
+    email = input("Email: ")
+for name in email_dictionary:
+    print(f"{name}({email_dictionary[name]})")
